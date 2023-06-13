5d482563f38d3eeb20b57c0b2a3cf0b13e48ab42
pandeyhimanshu1708 <97910793+pandeyhimanshu1708@users.noreply.github.com>
2023-06-13 07:44:29 +0000

    Basic Codes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const account_Id = 1234
+let accountEmail = "himanshu@gmail.com"
+var accountpassword = "12345"
+
+accountCity = "Bangalore"
+accountEmail = "himan@gmail.com"
+ /*
+ not use var as a variable
+ Always use a let keywords for a variable
+ */
+console.log(accountCity);
+console.log(account_Id);
+let accountState
+console.table([account_Id,accountEmail,accountpassword,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert("Hello")
+console.log(3+4);
+console.log("hello");
+let age = 23;
+null 
+
+// Data types:-
+// number => 2 to power of 53
+// bigint
+// boolean => true/false
+// null => Standalone value
+// undefined =>
+// Symbol =>unique
+
+
+// object
+console.log(typeof undefined);
+
+// object is a type of null
+// buyt undefined and others are same asative
