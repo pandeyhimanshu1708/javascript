2e12d26c8c6951006a88040c92783017d9fa90ce
pandeyhimanshu1708 <97910793+pandeyhimanshu1708@users.noreply.github.com>
2024-01-07 00:46:10 +0530

    Add files via upload

---
+++ dataTypesConversion.js
@@ -0,0 +1,48 @@
+// console.log("Hello Himanshu PAndey");
+
+
+let score = null;
+let scoreCard = undefined;
+
+// console.log(typeof(score));
+// console.log(typeof scoreCard);
+
+let valueInChange = Number(score);
+// console.log(typeof (valueInChange));
+// console.log(valueInChange);
+
+let valueCheck = Boolean(scoreCard);
+// console.log(typeof (valueCheck));
+// console.log(valueCheck); 
+
+let stringNumber = "17";
+// console.log(String(typeof stringNumber));
+// console.log(stringNumber);
+
+
+/*
+Note=>  for boolean
+1 => true,
+0 => false
+*/ 
+
+// ==================   Operations  =================================
+
+let value = 17;
+let negativeValue = -value;
+// console.log(negativeValue);
+
+// ================ Arithmetic Operations   ==============================
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 5); //power
+// console.log(2 / 2);
+// console.log(2 % 3);
+
+let firstName = "Himanshu";
+let lastName = " Pandey";
+let fullName = firstName + lastName
+console.log(firstName + lastName);
+console.log(fullName);
